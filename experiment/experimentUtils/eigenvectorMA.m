1365e55a61abdf81083b650d5e2ac52ca2041f04
Erik Keever <ekeever1@uoregon.edu>
2014-04-18 15:46:30 -0700

    Updates to eigenvector solvers for linear waves.

--- experiment/experimentUtils/eigenvectorMA.m
+++ experiment/experimentUtils/eigenvectorMA.m
@@ -1,39 +1,54 @@
-function ev = eigenvectorMA(rho, csq, v, b, k, wavetype)
+function [ev omega] = eigenvectorMA(rho, csq, v, b, k, wavetype)
 % This function returns the fast MA eigenvector coefficients [1 dvx dvy dvz dbx dby dbz]
 % associated with the plane wave posessing the given real k.
 % wavetype = [-2: fast bkwd, -1: slow bkwd, 1: slow fwd, 2: fast fwd]
 % where forward == re[w] > 0 and backwards == re[w] < 0
 % for exp[i(k.r - wt)]
 
+v=reshape(v,[3 1]);
+b=reshape(b,[3 1]);
+k=reshape(k,[1 3]);
+
+% Get into the DeHoffman-Teller frame s.t. v cross B = 0
+E_dht = cross(v, b);
+V_dht = cross(E_dht, b);
+
+v = v - V_dht;
+
 % Rotate into the plane such that vz/bz vanish
 
+zangle = atan2(b(3),b(2));
+
+R = [1 0 0; 0 cos(zangle) sin(zangle); 0 -sin(zangle) cos(zangle)];
+v=R*v;
+b=R*b;
+k=k*R; % such that k.r remains invariant as it must
+
 % Solve the dispersion relation
+bqA = rho;
+bqB = -k*k' * (b'*b + csq * rho);
+bqC = csq*(k*k')*(k*b)^2;
 
-lambda = dot(k,v) - w;
+% Select lambda such that dispersion relation is satisfied
+% Alternative: solveQuartic(bqA, 0, bqB, 0, bqC) gives same roots
+if abs(wavetype) == 2
+    lambda = -sign(wavetype)*sqrt((-bqB + sqrt(bqB*bqB - 4*bqA*bqC))/(2*bqA));
+else
+    lambda = -sign(wavetype)*sqrt((-bqB - sqrt(bqB*bqB - 4*bqA*bqC))/(2*bqA));
+end
 
-
-
-csq = flow.gamma*flow.P/flow.rho;
-
-lambda = kx*flow.vx + ky*flow.vy - w;
-kdb = kx*flow.bx + ky*flow.by;
-ksq = kx^2 + ky^2 + kz^2;
-bsq = flow.bx^2 + flow.by^2;
-rlsq = flow.rho*lambda^2;
-
-ev = zeros([7 1]);
-%ev = [0 0 0 0 0 0 0];
-
-% Coefficients of perturbed V[xyz]
+% Evalute the vector
+kdb = k*b;
+ksq = k*k';
+bsq = b'*b;
 
 ev(1) = 1;
+ev(2) = (csq*(b(1)*kdb*ksq/(lambda*rho) - k(1)*lambda))/(lambda^2*rho-bsq*ksq);
+ev(3) = (csq*(b(2)*kdb*ksq/(lambda*rho) - k(2)*lambda))/(lambda^2*rho-bsq*ksq);
+ev(4) = (csq*                             k(3)*lambda) /(bsq*ksq - rho*lambda^2);
+ev(5) = (csq*(k(1)*kdb - b(1)*ksq))/(bsq*ksq - rho*lambda^2);
+ev(6) = (csq*(k(2)*kdb - b(2)*ksq))/(bsq*ksq - rho*lambda^2);
+ev(7) = (csq* k(3)*kdb)            /(bsq*ksq - rho*lambda^2);
 
-ev(2) = (csq/(flow.rho*lambda))*(kx*rlsq - ksq*flow.bx*kdb)/(ksq*bsq - rlsq);
-ev(3) = (csq/(flow.rho*lambda))*(ky*rlsq - ksq*flow.by*kdb)/(ksq*bsq - rlsq);
-ev(4) = csq*kz*lambda/(ksq*bsq - rlsq);
-
-% Coefficient of perturbed B[xyz]
-ev(5) = csq*( kx*ky*flow.by - (ky^2+kz^2)*flow.bx)/(ksq*bsq - rlsq);
-ev(6) = -csq*(-kx*ky*flow.bx + (kx^2+kz^2)*flow.by)/(ksq*bsq - rlsq);
-ev(7) = csq*kz*kdb/(ksq*bsq - rlsq);
+omega = k*v - lambda;
 end
