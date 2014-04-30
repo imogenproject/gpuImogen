009dca3013f68d3ff22dc26b10dd0b00f3f7f92d
Erik Keever <ekeever1@uoregon.edu>
2014-04-30 16:23:22 -0700

    Fixes for automatic analysis of advection that did not get committed.

--- experiment/Advection/AdvectionAutoanalyze.m
+++ experiment/Advection/AdvectionAutoanalyze.m
@@ -1,4 +1,6 @@
-function domega = AdvectionAnalyzer(inputdir)
+function dw = AdvectionAutoanalyze(inputdir)
+% Given the directory storing an advection run,
+% automatically loads it up and confirms accuracy.
 
 cd(inputdir);
 % 2. Get box wavenumbers
@@ -6,15 +8,15 @@ load('SimInitializer_rank0.mat');
 
 if IC.ini.grid(3) == 1; DIMENSION = 2; else; DIMENSION = 3; end
 
-waveNumber = IC.ini.waveK;
-waveVector = IC.ini.waveDirection*2*pi*waveNumber ./ (IC.ini.dGrid.*IC.ini.grid); % Calculate realspace wavevectors = 2 pi N_i / L_i
+waveNumber = IC.ini.waveN;
+waveVector = IC.ini.waveK; % Calculate realspace wavevectors = 2 pi N_i / L_i
 A0 = IC.ini.waveAmplitude;
 
-% omega = c_s K; ASSUMES SONIC WAVE
-omega = IC.ini.waveDirection*sqrt(5/3)*norm(waveVector);
+omega = -IC.ini.waveOmega;
 
 % 3. Get from user savefile frame IDs
-%!ls | grep XYZ_rank0 | sed -e 's/_/ /g' -e 's/\./ /' | awk '/.*/ { printf("%s\n",$4); }' | awk '/^[0-9]/ { printf("%s ",$1); }' > tempnumbersfile
+%!ls | grep XYZ_rank0 | sed -e 's/_/ /g' -e 's/\./ /' | \
+%awk '/.*/ { printf("%s\n",$4); }' | awk '/^[0-9]/ { printf("%s ",$1); }' > tempnumbersfile
 %filelist = dlmread('tempnumbersfile');
 %filelist = [0 filelist];
 %!rm tempnumbersfile
@@ -30,21 +32,22 @@ fc = 2*dq(waveNumber(1)+1, waveNumber(2)+1, waveNumber(3)+1) / numel(f.mass);
 q0 = fftn(f0.mass);
 fc0= 2*q0(waveNumber(1)+1, waveNumber(2)+1, waveNumber(3)+1) / numel(f.mass);
 
+t = sum(f.time.history);
+
+% FIXME: The phase error calculation assumes the error is small (|| < pi)
 % Solve A1 = A0 exp(w_im t) with w_im the error in amplitude for w_re
-omega_im = log(abs(fc)/A0) / sum(f.time.history); 
-
-
+omega_im = log(abs(fc/fc0)) / t;
 % Solve phi(t) = (w0 + w_re) t for w_re the error in phase
-omega_re = -(angle(fc) - angle(fc0))/sum(f.time.history);
-%omega_re = (angle(fc) - angle(fc0)) / (sum(f.time.history)*IC.ini.numWavePeriods);
+omega_re = -(angle(fc) - angle(fc0))/t;
 
-fprintf('Wave transport analysis:\nTheory: q = q0 exp(-i w0 t)\n');
-fprintf('	c_s = %12.4e\n	k = 2 pi [%i %i %i]\n',sqrt(5/3), waveNumber(1),waveNumber(2),waveNumber(3));
-fprintf('        w0 = %12.4e\n',omega);
-fprintf('Reality: q = q0 exp(-i(w0 + dw)t)\n	t = %12.4e\n	Re[dw] = %12.4e,\n	Im[dw] = %12.4e\n',sum(f.time.history),omega_re, omega_im);
-fprintf('Normalized by w0: dw_hat = %12.4e %12.4ei\n', omega_re/omega, omega_im/omega);
-%fprintf('error omega w_err in exp(-i w_err t) is %12.4e %12.4ei\n', omega_im, omega_re);
+dw = [ omega_re, omega_im] / abs(omega);
 
-domega = [ omega_re/omega, omega_im/omega];
+fprintf('Wave transport analysis:\nTheory: q = q0 exp(-i w0 t) = q(t)\n');
+fprintf('\tc_s = %12.6e\n	k = 2 pi [%i %i %i]\n',sqrt(5/3), waveNumber(1),waveNumber(2),waveNumber(3));
+fprintf('\tw0 = %12.6e\n',omega);
+fprintf('\tt = %12.6e\n', sum(f.time.history));
+fprintf('Reality: q = q(t) exp(-i w dw t)\n');
+fprintf('\tRe[dw] / |w0| = %12.6e,\n\tIm[dw] / |w0| = %12.6e\n', dw(1), dw(2));
+fprintf('\tphase error radians/cycle = %12.6e,\n\tdecay e-folds/cycle = %12.6e\n', abs(omega*t*dw(1))/IC.ini.numWavePeriods, abs(dw(2))/IC.ini.numWavePeriods);
 
 end
