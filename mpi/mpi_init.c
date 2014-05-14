a810a380aa109ef21e68e3b7ccd89d724999388f
Erik Keever <ekeever1@uoregon.edu>
2014-05-14 14:57:41 -0700

    Modifies mpi_init to check MPI_Initialized and avoid fatal error by calling init twice

--- mpi/mpi_init.c
+++ mpi/mpi_init.c
@@ -9,12 +9,21 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 
 bool amLocked = mexIsLocked(); /* Our check for if this has been called already */
 
-if(amLocked) {
-  printf("WARNING: mpi_init() called, but has already been previously called.\n");
+if(nlhs == 1) {
+  mwSize dims[2]; dims[0] = 1; dims[1] = 1;
+  plhs[0] = mxCreateNumericArray(2, dims, mxDOUBLE_CLASS, mxREAL);
+  }
+
+int x, y;
+x = MPI_Initialized(&y);
+
+if(amLocked || y) {
+  if(nlhs == 1)  *(mxGetPr(plhs[0])) = 1.0;
   return;
   }
 
 mexLock();
-
 MPI_Init(NULL, NULL);
+
+if(nlhs == 1) *(mxGetPr(plhs[0])) = 0.0;
 }
