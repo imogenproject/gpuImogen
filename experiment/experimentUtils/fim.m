48325a9e34878147efd060d61a8ab1e699fb36e1
Erik Keever <ekeever1@uoregon.edu>
2014-05-20 16:18:54 -0700

    Commit working Fourier spectral integration routine. ~100pts -> 1e-8 errors in tests.

--- experiment/experimentUtils/fim.m
+++ experiment/experimentUtils/fim.m
@@ -7,11 +7,35 @@ f = func(x);
 h = x(2)-x(1);
 w=numel(x)*h;
 
-x = x - x(1); % Make a convenient transform to simplify
+xend = x(end)+h;
+xst  = x(1);
 
-%c0 = (f(end)+h*(f(end)-f(end-1)) - f(1))/w
-c0 = 0;%(f(end)-f(1))/w
-g = c0*x;
+x = x - x(1); % Make a convenient transform to simplify the polies
+
+% Evaluate f, f', f'' at the endpoints
+fE  = func(xend);
+ fpS = imag(func(xst+1i*eps))/eps;
+ fpE = imag(func(xend+1i*eps))/eps;
+  hvec = sqrt(1i)*[-1 -.5 .5 1]/32768;
+  fcmp = func(xst + hvec);
+  fppS = 1073741824*imag(64*(fcmp(3)+fcmp(2))-fcmp(1)-fcmp(4))/15;
+  fcmp = func(xend+ hvec);
+  fppE = 1073741824*imag(64*(fcmp(3)+fcmp(2))-fcmp(1)-fcmp(4))/15;
+
+% Order of continuity we wish to have
+switch 3;
+  case 0; c1 = 0; c2 = 0; c3 = 0;
+  case 1; c1 = (fE-f(1))/w;
+          c2 = 0; c3 = 0;
+  case 2; c1 = -(2*f(1) - 2*fE - w*fpS + w*fpE)./(2*w);
+          c2 = (fpE-fpS)./(2*w);
+          c3 = 0;
+  case 3; c1 = (12*fE - 6*fpE*w + 6*fpS*w + fppE*w.^2 - fppS*w.^2 - 12*f(1))./(12*w);
+          c2 = (2*fpE - 2*fpS - fppE*w + fppS*w)./(4*w);
+          c3 = (fppE - fppS)./(6*w);
+end
+
+g = x.*(c1 + x.*(c2 + c3*x));
 
 f = f - g;
 
@@ -25,12 +49,12 @@ else
 end
 
 % Peel off 0th Fourier mode which is not integrable this way
-fbar = ftilde(1)/N;
+c0 = ftilde(1)/N;
 ftilde(1) = 0;
 
 F = real(w*ifft(ftilde./coefs)/(2*pi));
 
-result = x.*(fbar + x*c0/2) + F;
+result = x.*(c0 + x.*(c1/2 + x.*(c2/3+x*c3/4))) + F;
 
 % Return the promised value
 result = result - result(1);
