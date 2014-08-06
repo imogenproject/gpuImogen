206dfd39c1d13574f1593a2805104b8b75d1b3c3
Erik Keever <ekeever1@uoregon.edu>
2014-08-06 16:12:33 -0700

    Partial parallelization work on RT initializer

--- experiment/RayleighTaylor/RayleighTaylorInitializer.m
+++ experiment/RayleighTaylor/RayleighTaylorInitializer.m
@@ -88,49 +88,61 @@ classdef RayleighTaylorInitializer < Initializer
 
             %--- Ensure that the grid dimensions are even. ---%
             %       Even grid size means that the star will be correctly placed in the center cell.
+            GIS = GlobalIndexSemantics();
             potentialField = [];
             selfGravity = [];
-
-            for i=1:3
-                if (bitget(obj.grid(i),1) && obj.grid(i) > 1);  obj.grid(i) = obj.grid(i) + 1; end
-            end
+            %obj.grid -> GIS.pMySize; this enables parallelization
             
+            [X Y Z] = GIS.ndgridSetXYZ();
             obj.bcMode.grav.y = ENUM.BCMODE_FADE;
             
             % Establish low density below, high density above
-            mass = zeros(obj.grid);
-            mass(:,1:obj.grid(2)/2,:)       = obj.rhoBottom;
-            mass(:,(obj.grid(2)/2+1):end,:) = obj.rhoTop;
+            %mass = zeros(GIS.pMySize);
+            %mass(:,1:GIS.pMySize(2)/2,:)       = obj.rhoBottom;
+            %mass(:,(GIS.pMySize(2)/2+1):end,:) = obj.rhoTop;
 
             % Compute pressure & gravity field to establish hydrostatic equilibrium
-            [Xg Yg Zg] = ndgrid(1:obj.grid(1), 1:obj.grid(2), 1:obj.grid(3));
+            %[Xg Yg Zg] = ndgrid(1:GIS.pMySize(1), 1:GIS.pMySize(2), 1:GIS.pMySize(3));
+            %obj.dGrid   = [.5 (.5*GIS.pMySize(2)/GIS.pMySize(1)) .5] ./ GIS.pMySize;
+            %Xg = Xg * obj.dGrid(1);
+            %Yg = Yg * obj.dGrid(2);
+            %Zg = Zg * obj.dGrid(3);
+            %Y0 = Yg(1,round(end/2),1);
+            
             obj.dGrid   = [.5 (.5*obj.grid(2)/obj.grid(1)) .5] ./ obj.grid;
-            Xg = Xg * obj.dGrid(1);
-            Yg = Yg * obj.dGrid(2);
-            Zg = Zg * obj.dGrid(3);
-            Y0 = Yg(1,round(end/2),1);
+            X = X * obj.dGrid(1);
+            Y = Y * obj.dGrid(2);
+            Z = Z * obj.dGrid(3);
+            Y0 = .5*obj.dGrid(2)*obj.grid(2);
+            
+            % Establish low density below, high density above
+            mass             = zeros(GIS.pMySize);
+            mass(Y < Y0)     = obj.rhoBottom;
+            mass(Y >= Y0)    = obj.rhoTop;
 
             % Gas pressure to balance gravity
             ener = (obj.P0 - obj.gravity.constant * (cumsum(mass,2)-mass(1,1,1)) * obj.dGrid(2) ) / (obj.gamma-1);
+            %copmute energy density locally in each cell:
+            %ener = (pressure@cell - obj.gravity.constant * (cumsum(mass,2)-mass(1,1,1)) * obj.dGrid(2) ) / (obj.gamma-1)
 
-            obj.gravity.fixedPotential = obj.gravity.constant * Yg;
+            obj.gravity.fixedPotential = obj.gravity.constant * Y;
 
-            mom         = zeros([3 obj.grid]);
+            mom         = zeros([3 GIS.pMySize]);
             obj.thresholdMass = .0001 * obj.rhoBottom;
             obj.minMass = .5*obj.thresholdMass;
-            mag         = zeros([3 obj.grid]);
+            mag         = zeros([3 GIS.pMySize]);
 
             if obj.randomPert == 0
                 % Set initial velocity perturbation
                 % Vy = A [1+cos(2pi Kx x/Lx)] [1+cos(2pi Ky y/Ly)]/4
-                if obj.grid(3) == 1;
-                    mom(2,:,:,:) = obj.pertAmplitude * (1+cos(2*pi*obj.Kx*Xg/.5)) .* (1+cos(2*pi*obj.Ky*(Yg-Y0)/1.5))/ 4;
+                if GIS.pMySize(3) == 1;
+                    mom(2,:,:,:) = obj.pertAmplitude * (1+cos(2*pi*obj.Kx*X/.5)) .* (1+cos(2*pi*obj.Ky*(Y-Y0)/1.5))/ 4;
                 else
-                    mom(2,:,:,:) = obj.pertAmplitude * (1+cos(2*pi*obj.Kx*Xg/.5)) .* (1+cos(2*pi*obj.Ky*(Yg-Y0)/1.5)) .* (1+cos(2*pi*obj.Kz*Zg/.5))/ 8;
+                    mom(2,:,:,:) = obj.pertAmplitude * (1+cos(2*pi*obj.Kx*X/.5)) .* (1+cos(2*pi*obj.Ky*(Y-Y0)/1.5)) .* (1+cos(2*pi*obj.Kz*Z/.5))/ 8;
                 end
             else
-                w = (rand([obj.grid(1) obj.grid(3)])-.5) * obj.pertAmplitude;
-                for y = 1:obj.grid(2); mom(2,:,y,:) = w; end
+                w = (rand([GIS.pMySize(1) GIS.pMySize(3)])-.5) * obj.pertAmplitude;
+                for y = 1:GIS.pMySize(2); mom(2,:,y,:) = w; end
             end
 
             mom(2,:,:,:) = squeeze(mom(2,:,:,:)).*mass;
@@ -149,32 +161,32 @@ classdef RayleighTaylorInitializer < Initializer
 
 %statics = [];            return;
             
-            yfix = [(obj.grid(2)-3):obj.grid(2)];
+            yfix = [(GIS.pMySize(2)-3):GIS.pMySize(2)];
             
             %%%% Force top and bottom edges to be static
             statics.values  = [0, obj.rhoBottom, obj.rhoTop, ener(1,3,1), ener(1,end-3,1), obj.Bx, obj.Bz];
-            statics.mass.s  = uint8(zeros(obj.grid));
+            statics.mass.s  = uint8(zeros(GIS.pMySize));
             statics.mass.s(:,1:3,:) = 2;
             statics.mass.s(:,yfix,:) = 3;
 
-            statics.ener.s  = uint8(zeros(obj.grid));
+            statics.ener.s  = uint8(zeros(GIS.pMySize));
             statics.ener.s(:,1:3,:) = 4;
             statics.ener.s(:,yfix,:) = 5;
 
             fields = {'x', 'y', 'z'};
             for i=1:3
-                statics.mom.s.(fields{i}) = uint8(zeros(obj.grid));
+                statics.mom.s.(fields{i}) = uint8(zeros(GIS.pMySize));
 		statics.mom.s.(fields{i})(:,1:3,:) = 1;
                 statics.mom.s.(fields{i})(:,yfix,:) = 1;
             end
 
 	    i = 1;
-            statics.mag.s.(fields{i}) = uint8(zeros(obj.grid));
+            statics.mag.s.(fields{i}) = uint8(zeros(GIS.pMySize));
             statics.mag.s.(fields{i})(:,1:3,:) = 6;
             statics.mag.s.(fields{i})(:,yfix,:) = 6;
 
             i = 3;
-            statics.mag.s.(fields{i}) = uint8(zeros(obj.grid));
+            statics.mag.s.(fields{i}) = uint8(zeros(GIS.pMySize));
             statics.mag.s.(fields{i})(:,1:3,:) = 7;
             statics.mag.s.(fields{i})(:,yfix,:) = 7;
 
