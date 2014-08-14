c363bce5939f4d7d7ef9019b052906bc46e2f459
Erik Keever <ekeever1@uoregon.edu>
2014-08-13 17:30:48 -0700

    Final changes to create an in-situ analysis capability; Modify implosion test to test this.

--- run/run_Implosion.m
+++ run/run_Implosion.m
@@ -3,20 +3,25 @@
 %-- Initialize Imogen directory ---%
 starterRun();
 
-grid = [2048 2048 1];
+grid = [512 512 1];
 GIS = GlobalIndexSemantics(); GIS.setup(grid);
 
 %--- Initialize test ---%
 run                 = ImplosionInitializer(grid);
-run.iterMax         = 30000;
+run.iterMax         = 10000;
 
 run.direction       = ImplosionInitializer.X;
-run.image.interval  = 50;
+run.image.interval  = 100;
 run.image.mass      = true;
 run.activeSlices.xy = true;
 run.info            = 'Implosion symmetry test';
 run.notes           = '';
+run.ppSave.dim2     = 10;
 
+% The following tracks the asymmetry of the implosion over time
+run.useInSituAnalysis = 1;
+run.inSituHandle = @ImplosionAnalyzer.getInstance;
+run.stepsPerInSitu = 100;
 
 %--- Run tests ---%
 if (true)
