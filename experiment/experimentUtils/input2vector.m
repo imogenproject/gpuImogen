d14e1456cfe37ffcbd97be9d2e7ea396dad860ec
Erik Keever <ekeever1@uoregon.edu>
2015-03-05 14:54:33 -0800

    Add input2vector utility required by several initializers.

---
+++ experiment/experimentUtils/input2vector.m
@@ -0,0 +1,22 @@
+function [V err] = input2vector(user, N, padvalue, makeint)
+% input2vector(user, N, padvalue, makeint) is a one-stop shop
+% covering many common cases of getting an N-vector
+%> user: user's input
+%> N: number of elments in return vector
+%> padvalue: V(++numel(user):N) is padded with this
+%> makeint: if nonzero, round()s result
+%< V: return vector
+%< err: Any 
+
+V = user;
+
+% Make sure we have N elements
+V((numel(user)+1):N) = padvalue(1);
+
+% Crop to N if too many
+if numel(V) > N; V = V(1:N); end
+
+if makeint; V = round(V); end
+
+
+end
