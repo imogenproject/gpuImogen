c1ae9c856a005968f74cf696c8903ad31674ae5d
Erik Keever <ekeever1@uoregon.edu>
2015-05-13 13:08:30 -0700

    Add a third optional Nini parameter (def = 1) to lowPass so the filter starts at the average of the first Nini points

--- external/lowPass.m
+++ external/lowPass.m
@@ -1,6 +1,8 @@
-function lp = lowPass(f, c)
+function lp = lowPass(f, c, Nini)
 
-lp(1) = f(1);
+if nargin < 3; Nini = 1; end
+
+lp(1) = mean(f(1:Nini));
 
 for t = 2:numel(f)
     lp(t) = lp(t-1) + c*(f(t) - lp(t-1));
