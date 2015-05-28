ced99432a598ebdbcac3aceec501a2ca85447903
Erik Keever <ekeever1@uoregon.edu>
2015-05-28 15:36:43 -0700

    Fix some bugs in the savefile portal, add documentation.

---
+++ classes/SavefilePortal.m
@@ -0,0 +1,156 @@
+classdef SavefilePortal < handle
+    %___________________________________________________________________________________________________
+    
+    %===================================================================================================
+    properties (Constant = true, Transient = true) %                 C O N S T A N T         [P]
+    end%CONSTANT
+    
+    %===================================================================================================
+    properties (SetAccess = public, GetAccess = public) %                           P U B L I C  [P]
+    end %PUBLIC
+    
+    %===================================================================================================
+    properties (SetAccess = protected, GetAccess = protected) %                P R O T E C T E D [P]
+        savefileDirectory;
+        currentFrame;
+        typeToLoad;
+
+        savefileList;
+        strnames={'X','Y','Z','XY','XZ','YZ','XYZ'};
+
+	directoryStack;
+    end %PROTECTED
+    
+    %===================================================================================================
+    methods %                                                                     G E T / S E T  [M]
+    end%GET/SET
+    
+    %===================================================================================================
+    methods (Access = public) %                                                     P U B L I C  [M]
+        function self = SavefilePortal(wd)
+            % SavefilePortal(directory) provides a unified way to access
+            % all Imogen save data
+            if nargin == 0; wd = pwd(); end
+
+            self.changeDirectory(wd);
+
+            self.typeToLoad = 7;
+        end
+
+        function changeDirectory(self, nwd)
+            % changeDirectory(nwd) makes the portal operate on the New
+            % Working Directory; Resets the portal (all frames -> first).
+            % Does not corrupt caller's PWD.
+            self.savefileDirectory = nwd;
+	    self.pushdir(nwd);
+            
+	    self.currentFrame = [0 0 0 0 0 0 0];
+            self.savefileList = enumerateSavefiles();
+
+            self.popdir();
+        end
+
+        function setFrametype(self, id)
+        % setFrametype(id) sets the frame used to X, Y, Z, XY, XZ, YZ, XYZ
+        % for id values 1-7 respectively.
+	    if id < 1; id = 7; disp('Invalid ID; defaulted to XYZ (7)'); end
+	    if id > 7; id = 7; disp('Invalid ID; defaulted to XYZ (7)'); end
+            self.typeToLoad = id;
+        end
+        function accessX(self); self.setFrametype(1); end
+        function accessY(self); self.setFrametype(2); end
+        function accessZ(self); self.setFrametype(3); end
+        function accessXY(self); self.setFrametype(4); end
+        function accessXZ(self); self.setFrametype(5); end
+        function accessYZ(self); self.setFrametype(6); end
+        function accessXYZ(self); self.setFrametype(7); end
+
+        function F = nextFrame(self)
+            % F = nextFrame() returns the next Imogen saveframe of the
+            % currently selected type
+	    self.pushdir(self.savefileDirectory);
+            n = self.currentFrame(self.typeToLoad) + 1;
+            b = getfield(self.savefileList,self.strnames{self.typeToLoad});
+
+            if n > numel(b)
+                n = n-1;
+            end
+            self.currentFrame(self.typeToLoad) = n;
+
+            F = util_LoadWholeFrame(self.typeToLoad,self.savefileList.misc.padlen, b(n));
+	    self.popdir();
+        end
+
+        function F = previousFrame(self)
+            % F = previousFrame() returns the previous Imogen saveframe of
+            % the current type
+            
+            n = self.currentFrame(self.typeToLoad)-1;
+            b = getfield(self.savefileList,self.strnames{self.typeToLoad});
+            if n <= 0; n = 1; end
+            self.currentFrame(self.typeToLoad) = n;
+
+            self.pushdir(self.savefileDirectory);
+            F = util_LoadWholeFrame(self.typeToLoad,self.savefileList.misc.padlen, b(n));
+            self.popdir();
+        end
+
+        function F = setFrame(self, f)
+            % F = setFrame(n) jumps to the indicated frame of the current
+            % type; Automatically clamps to [1 ... #frames]
+            if f < 1; f = 1; end
+            if f >= self.numFrames();
+                f = self.numFrames();
+            end
+            
+            b = getfield(self.savefileList,self.strnames{self.typeToLoad});
+            self.currentFrame(self.typeToLoad) = f;
+            
+            self.pushdir(self.savefileDirectory);
+            F = util_LoadWholeFrame(self.typeToLoad, self.savefileList.misc.padlen, b(f));
+            self.popdir();
+        end
+
+        function rewind(self)
+            % Resets the current frame to the first
+    	    self.currentFrame(self.typeToLoad) = 0;
+        end
+    
+       function arewe = atLastFrame(self)
+           % true if the portal is currently aimed at the last frame of the
+           % current type, otherwise false
+            arewe = (self.currentFrame(self.typeToLoad) == self.numFrames());
+       end
+
+       function n = tellFrame(self)
+           % N = tellFrame() indicates which frame was the last
+           % loaded/returned
+            n = self.currentFrame(self.typeToLoad);
+       end
+
+	function n = numFrames(self)
+        % n = numFrames() returns how many frames of the current type are
+        % accessible in the current directory
+	    n = numel(getfield(self.savefileList,self.strnames{self.typeToLoad}));
+	end
+
+    end%PUBLIC
+    
+    %===================================================================================================
+    methods (Access = protected) %                                      P R O T E C T E D    [M]
+	function pushdir(self, D)
+	    self.directoryStack{end+1} = pwd();
+	    cd(D);
+	end
+
+	function popdir(self)
+	    cd(self.directoryStack{end});
+	    self.directoryStack = self.directoryStack(1:(end-1));
+	end
+    end%PROTECTED
+    
+    %===================================================================================================
+    methods (Static = true) %                                                 S T A T I C    [M]
+    end%PROTECTED
+    
+end%CLASS
