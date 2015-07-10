016748bc7855747f268bd651bbc5dafaec22e776
Erik Keever <ekeever1@uoregon.edu>
2015-07-10 12:29:23 -0700

    Substantially larger and more powerful unit for confirming fundamental, basic functionality of cuda/matlab interface.

--- gpuclass/basicTest.m
+++ gpuclass/basicTest.m
@@ -1,38 +1,59 @@
+function result = basicTest(REZ)
+
 x = GPUManager.getInstance();
 
-x.init([0], 1, 1);
-disp('Testing successful upload/download with device, single GPU');
-A = rand([28 319]);
-Ad = GPU_Type(A);
-B = max(max(abs(Ad.array - A)));
-if B ~= 0; error('Scalar GPU ul/dl: Returned data failed to be exactly equal to original. Fail.'); end
+if nargin == 0; REZ = [449 875]; end
 
-x.init([0 0], 1, 1);
-disp('Testing successful upload/download with device, two GPUs');
-A = rand([28 319]);
+
+x.init([0], 1, 1);
+disp('Testing upload/download to one GPU...');
+A = rand(REZ);
 Ad = GPU_Type(A);
 B = max(max(abs(Ad.array - A)));
-if B ~= 0; error('Partitioned GPU ul/dl: Returned data failed to be exactly equal to original. Fail.'); end
+if B ~= 0; error('Scalar GPU ul/dl: Returned data failed to be exactly equal to original. Fail.');
+    else; disp('Success!'); end
 
 x.init([0 2], 1, 1);
-disp('Testing successful upload/download with device, two different GPUs');
-A = rand([281 319]);
+disp('Testing upload/download to two GPUs...');
+A = rand(REZ);
 Ad = GPU_Type(A);
-B = max(max(abs(Ad.array - A)));
-if B ~= 0; error('Multi-device ul/dl: Returned data failed to be exactly equal to original. Fail.'); end
+theta = Ad.array;
+B = max(max(abs(theta - A)));
+if B ~= 0; error('Multi-device ul/dl: Returned data failed to be exactly equal to original. Fail.');
+    else; disp('Success!'); end
 
+disp('Attempting to copy multi-device GPU array into novel pointer (GPU_clone)...');
 Cd = GPU_clone(Ad);
 B = max(max(abs(GPU_download(Cd) - Ad.array)));
-if B ~= 0; error('GPU_clone returned bad data.'); end
+if B ~= 0; error('GPU_clone does not work.');
+    else; disp('Success!'); end
 
 GPU_free(Cd);
 
-Cd = GPU_Type(zeros([281 319]) );
+disp('Attempting to copy multi-device GPU array between existing pointers (GPU_copy)...');
+Cd = GPU_Type(rand(REZ) );
 GPU_copy(Cd.GPU_MemPtr, Ad.GPU_MemPtr);
 B = max(max(abs(GPU_download(Cd) - Ad.array)));
 if B ~= 0; error('GPU_copy does not work.'); end
 
+disp('--- Exercising slab functionality ---');
+
+disp('Attempting to extend array to have three slabs...');
 Ad.createSlabs(3);
 
-Btag = GPU_getslab(Ad, 1);
+disp('Accessing slab 1...');
+Btag = GPU_Type(GPU_getslab(Ad, 1));
+
+disp('Setting slab value...');
+B = rand(REZ);
+Btag.array = B;
+
+disp('Downloading slab...');
+diff = max(max(max(abs(Btag.array - B))));
+if diff ~= 0; error('Transfer to/from slab failed!'); 
+    else; disp('Successful!'); end
+
+result = 0;
+
+end
 
