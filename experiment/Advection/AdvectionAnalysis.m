a88f06b7cd9c1ae59b2970876ee4c1e4d1dd95af
Erik Keever <ekeever1@uoregon.edu>
2015-09-11 18:20:35 -0700

    Update advection analyzer to permit automatic parallel analysis

--- experiment/Advection/AdvectionAnalysis.m
+++ experiment/Advection/AdvectionAnalysis.m
@@ -1,4 +1,4 @@
-function autopsy = AdvectionAnalysis(directory)
+function autopsy = AdvectionAnalysis(directory, runParallel);
 
 dir0 = pwd();
 cd(directory);
@@ -6,6 +6,13 @@ load('SimInitializer_rank0.mat','IC');
 cd(dir0);
 
 S = SavefilePortal(directory);
+
+% If called by Imogen, this will be true (such that the parallel process can run in parallel)
+% If the savefile portal's parallel switch is flipped on, loaded frames are only this rank's
+% frame segment instead of the reassembled entire frame.
+if nargin < 2; runParallel = 0; end
+
+S.setParallelMode(runParallel);
 S.setFrametype(7); % Access 3D data frame by frame
 
 % Pick out the wavevector
@@ -34,6 +41,8 @@ rhoerr_L1 = []; rhoerr_L2 = [];
 velerr_L1 = []; velerr_L2 = [];
 frameT = [];
 
+GIS = GlobalIndexSemantics(); 
+
 % Since we know the initial function is a sine, write down the critical
 % time
 tCritical = 2/((IC.ini.gamma + 1)*c0*Kmag*IC.ini.amplitude);
@@ -44,13 +53,15 @@ for N = 1:S.numFrames();
     t = sum(F.time.history);
     % Compute the displacement of a reference wave through circular BCs 
     % Parameterized by original phase
-    origMap = CharacteristicAnalysis1D(0:.0001:.9999, 1, IC.ini.pDensity, c0, machParallel, IC.ini.gamma, IC.ini.amplitude*cos(2*pi*(0:.0001:.9999)), t);
+    backMap = CharacteristicAnalysis1D(0:.0001:.9999, 1, IC.ini.pDensity, c0, machParallel, IC.ini.gamma, IC.ini.amplitude*cos(2*pi*(0:.0001:.9999)), t);
 
     % Map this onto the full 3D space by referring to original phases
-    rhoAnalytic = interp1(2*pi*(0:.0001:.9999), origMap, mod(KdotX,2*pi),'cubic');
+    rhoAnalytic = interp1(2*pi*(0:.0001:.9999), backMap, mod(KdotX,2*pi),'cubic');
 
-    % The moment of truth
-    delta = (rhoAnalytic - F.mass);
+    % The moment of truth: calculate the 1- and 2-norms
+    delta = rhoAnalytic - F.mass;
+    if runParallel; delta = GIS.withoutHalo(delta); end
+
     if sum(F.time.history) >= tCritical;
         disp(['At frame', S.tellFrame(), ' time ', num2str(t), ' exceeded tCritical=', num2str(tCritical),'; Analysis ended.'])
         break;
@@ -58,8 +69,8 @@ for N = 1:S.numFrames();
     
     frameT(end+1) = t;
 
-    rhoerr_L1(N) = norm(delta,1) / numel(delta);
-    rhoerr_L2(N) = norm(delta,2) / sqrt(numel(delta));
+    rhoerr_L1(N) =      mpi_sum(norm(delta,1)  ) / mpi_sum(numel(delta)) ;
+    rhoerr_L2(N) = sqrt(mpi_sum(norm(delta,2)^2) / mpi_sum(numel(delta)));
 end
 
 autopsy.T = frameT;
@@ -69,7 +80,11 @@ autopsy.wavenumber = IC.ini.pWavenumber;
 autopsy.rhoL1 = rhoerr_L1;
 autopsy.rhoL2 = rhoerr_L2;
 
-save('analyzer_results.mat','autopsy');
+if mpi_amirank0()
+    cd(directory);
+    save('analyzer_results.mat','autopsy');
+    cd(dir0);
+end
 
 return
 
