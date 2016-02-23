09eb90830ba9e8c7e475e9cb229735cdfec9287a
Erik Keever <ekeever1@uoregon.edu>
2016-02-22 17:02:51 -0800

    Fix critical flaw where Matlab hi-level NetCDF library accumulates open file handles w/o limit by rewriting to use lo-level library; Flaw can accumulate enough FDs to crash NFS filesystems.

--- utils/util_Frame2NCD.m
+++ utils/util_Frame2NCD.m
@@ -8,6 +8,126 @@ d3_fixed = {'nx','ny','nz'};
 th = frame.time.history;
 if isempty(frame.time.history); th = 0; end
 
+% open ncd handle
+ncid = netcdf.create(nfile, '64BIT_OFFSET');
+
+% FIXME: check for r/w failure here
+
+%nc_def_dim(ncid, "dimname", value, &dimid);
+vecdim = netcdf.defDim(ncid, '3elem', 3); % this is dumb but it's historically locked...
+scaldim= netcdf.defDim(ncid, '1elem', 1);
+
+% Define time substructure dimensions
+dthist = netcdf.defDim(ncid, 'dthist', numel(th));
+tinfo  = netcdf.defDim(ncid, 'tinfo', 5);
+tstart = netcdf.defDim(ncid, 'tstart', length(frame.time.started));
+
+% Define parallel substructure dimensions
+geomnx = netcdf.defDim(ncid, 'geomnx', size(frame.parallel.geometry,1));
+geomny = netcdf.defDim(ncid, 'geomny', size(frame.parallel.geometry,2));
+geomnz = netcdf.defDim(ncid, 'geomnz', size(frame.parallel.geometry,3));
+
+% define small parameter dimensions
+aboutstr   = netcdf.defDim(ncid, 'aboutstr', length(frame.about));
+versionstr = netcdf.defDim(ncid, 'versionstr', length(frame.ver));
+
+% Define dgrid dimensions
+dgridnx = netcdf.defDim(ncid, 'dgridnx', size(frame.dGrid{1},1));
+dgridny = netcdf.defDim(ncid, 'dgridny', size(frame.dGrid{2},2));
+dgridnz = netcdf.defDim(ncid, 'dgridnz', size(frame.dGrid{3},3));
+simdim  = netcdf.defDim(ncid, 'simdim', length(frame.dim)); % good lord...
+
+% define grid dimensions
+nx = netcdf.defDim(ncid, 'nx', size(frame.mass,1));
+ny = netcdf.defDim(ncid, 'ny', size(frame.mass,2));
+nz = netcdf.defDim(ncid, 'nz', size(frame.mass,3));
+
+%nc_def_var(ncid, "varname", TYPE, numdims, ptr_to_dimid, &varid);
+% DEFINE ALL VARIABLES
+% Define time info substructure
+timeinfo_hist   = netcdf.defVar(ncid, 'timeinfo_hist', 'double', dthist);
+timeinfo_scals  = netcdf.defVar(ncid, 'timeinfo_scals', 'double', tinfo);
+timeinfo_tstart = netcdf.defVar(ncid, 'timeinfo_tstart', 'char', tstart);
+
+% Define parallel substructure
+parallel_geom   = netcdf.defVar(ncid, 'parallel_geom', 'double', [geomnx geomny geomnz]);
+parallel_gdims  = netcdf.defVar(ncid, 'parallel_gdims', 'double', vecdim);
+parallel_offset = netcdf.defVar(ncid, 'parallel_offset', 'double', vecdim);
+
+% Define small parameters
+gammavar   = netcdf.defVar(ncid, 'gamma', 'double', scaldim);
+aboutvar   = netcdf.defVar(ncid, 'about', 'char', aboutstr);
+versionvar = netcdf.defVar(ncid, 'version', 'char', versionstr);
+
+% Define dgrid parameters
+dgrid_x = netcdf.defVar(ncid, 'dgrid_x', 'double', dgridnx);
+dgrid_y = netcdf.defVar(ncid, 'dgrid_y', 'double', dgridny);
+dgrid_z = netcdf.defVar(ncid, 'dgrid_z', 'double', dgridnz);
+dimvar  = netcdf.defVar(ncid, 'dim', 'char', simdim); % good grief
+
+% Define main data variables
+mass = netcdf.defVar(ncid, 'mass', 'double', [nx ny nz]);
+momX = netcdf.defVar(ncid, 'momX', 'double', [nx ny nz]);
+momY = netcdf.defVar(ncid, 'momY', 'double', [nx ny nz]);
+momZ = netcdf.defVar(ncid, 'momZ', 'double', [nx ny nz]);
+ener = netcdf.defVar(ncid, 'ener', 'double', [nx ny nz]);
+
+magstatus = netcdf.defVar(ncid, 'magstatus', 'double', scaldim);
+if isempty(frame.magX) || numel(frame.magX) ~= numel(frame.mass)
+    % Defines a placeholder that marks magnetic arrays as absent
+else
+    magX = netcdf.defVar(ncid, 'magX', 'double', [nx ny nz]);
+    magY = netcdf.defVar(ncid, 'magY', 'double', [nx ny nz]);
+    magZ = netcdf.defVar(ncid, 'magZ', 'double', [nx ny nz]);
+end
+
+netcdf.endDef(ncid);
+
+%nc_put_var_TYPE(ncid, varid, &data);
+% Serialize time substructure
+netcdf.putVar(ncid, timeinfo_hist, th);
+netcdf.putVar(ncid, timeinfo_scals, [frame.time.time;frame.time.iterMax;frame.time.timeMax;frame.time.wallMax;frame.time.iteration]);
+netcdf.putVar(ncid, timeinfo_tstart, frame.time.started);
+
+% Serialize geometry info
+netcdf.putVar(ncid, parallel_geom, frame.parallel.geometry);
+netcdf.putVar(ncid, parallel_gdims, frame.parallel.globalDims);
+netcdf.putVar(ncid, parallel_offset, frame.parallel.myOffset);
+
+% Serialize parameters and other small stuff
+netcdf.putVar(ncid, gammavar, frame.gamma);
+netcdf.putVar(ncid, aboutvar, frame.about);
+netcdf.putVar(ncid, versionvar, frame.ver);
+
+% Write dGrid information
+netcdf.putVar(ncid, dgrid_x, frame.dGrid{1});
+netcdf.putVar(ncid, dgrid_y, frame.dGrid{2});
+netcdf.putVar(ncid, dgrid_z, frame.dGrid{3});
+netcdf.putVar(ncid, dimvar, frame.dim); % ugh good grief aagain
+
+netcdf.putVar(ncid, mass, frame.mass);
+netcdf.putVar(ncid, momX, frame.momX);
+netcdf.putVar(ncid, momY, frame.momY);
+netcdf.putVar(ncid, momZ, frame.momZ);
+netcdf.putVar(ncid, ener, frame.ener);
+
+if isempty(frame.magX) || numel(frame.magX) ~= numel(frame.mass)
+    netcdf.putVar(ncid, magstatus, 0);
+else
+    netcdf.putVar(ncid, magstatus, 1);
+    netcdf.putVar(ncid, magX, frame.magX);
+    netcdf.putVar(ncid, magY, frame.magY);
+    netcdf.putVar(ncid, magZ, frame.magZ);
+end
+
+netcdf.close(ncid);
+
+return;
+
+% Old file-handle-leaking code
+% DO NOT USE
+% Holy Christ it's unbelievable that the high-level interface doesn't have a 'close'
+
 % Serialize time substructure
 nccreate(nfile,'timeinfo_hist','Dimensions',{'dthist',numel(th)});
 ncwrite(nfile,'timeinfo_hist',th);
@@ -93,6 +213,7 @@ else
     nccreate(nfile,'magZ','Dimensions',d3_fixed);
     ncwrite(nfile,'magZ', frame.magZ);
 end
+
 
 
 end
