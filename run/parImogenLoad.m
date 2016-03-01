06b0a2284dc7edbf5457751a20bc5e9b550fe78c
Erik Keever <ekeever1@uoregon.edu>
2016-03-01 15:56:23 -0800

    Update imogen loader to accept 'gputest' as a magic key to run the gpuclass/tortureTest.m script instead of a real simulation.

--- run/parImogenLoad.m
+++ run/parImogenLoad.m
@@ -10,25 +10,42 @@ function parImogenLoad(runFile, logFile, alias, gpuSet, nofinalize)
     failed = starterRun(gpuSet);
 
     if failed == 0;
-        runFile = strrep(runFile,'.m','');
-        assignin('base','logFile',logFile);
-        assignin('base','alias',alias);
-    
-        if nargin < 5;
-            if mpi_myrank() == 0;
-                fprintf('No 5th argument: Assuming run is scripted, will shut down everything and mpi_finalize at completion.\n'); end
-                shutDownEverything = 1;
-        end
-
-        try
-            eval(runFile);
-        catch ME
-            fprintf('FATAL: Runfile has thrown an exception back to loader.\nRANK %i IS ABORTING JOB!\nException report follows:\n', mpi_myrank());
-            prettyprintException(ME);
-            if shutDownEverything;
-                fprintf('Run is non-interactive: Invoking MPI_Abort() to avoid hanging job.\n');
-                mpi_abort();
+        % Accept magic name 'gputest' to simply hammer the GPUs with work rather than run Imogen
+        if strcmp(runFile,'gputest')
+            shutDownEverything = 1;
+            cd gpuclass; % tortureTest expects to be run from here
+            try
+                tortureTest(gpuSet, 'y', 4);
+            catch crap
+                prettyprintException(crap, 1, 'One or more ranks have failed GPU tests! ECC -> hardware problem. Other -> Bad code checked into git?');
+                failed = 1;
             end
+            
+            failed = mpi_max(failed);
+            if(failed) mpi_abort(); end
+        else
+
+            runFile = strrep(runFile,'.m','');
+            assignin('base','logFile',logFile);
+            assignin('base','alias',alias);
+    
+            if nargin < 5;
+                if mpi_myrank() == 0;
+                    fprintf('No 5th argument: Assuming run is scripted, will shut down everything and mpi_finalize at completion.\n'); end
+                shutDownEverything = 1;
+            end
+
+            try
+                eval(runFile);
+            catch ME
+                fprintf('FATAL: Runfile has thrown an exception back to loader.\nRANK %i IS ABORTING JOB!\nException report follows:\n', mpi_myrank());
+                prettyprintException(ME);
+                if shutDownEverything;
+                    fprintf('Run is non-interactive: Invoking MPI_Abort() to avoid hanging job.\n');
+                    mpi_abort();
+                end
+            end
+
         end
     else
         shutDownEverything = 1;
