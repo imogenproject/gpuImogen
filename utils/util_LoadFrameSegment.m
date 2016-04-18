43dca9d601bb3d86b141eed3d880d1c71eefc291
Erik Keever <ekeever1@uoregon.edu>
2016-04-18 16:42:25 -0700

    Change frame segment loader to accept numeric 1st and last frames as well as symbolic _START and _END (save code is changed to produce numeric)

--- utils/util_LoadFrameSegment.m
+++ utils/util_LoadFrameSegment.m
@@ -10,32 +10,13 @@ if isa(basename,'double')
     end
 end
 
-    if frameno == 0; f1 = sprintf('%s_rank%i_START.nc', basename,rank);
-                     f2 = sprintf('%s_rank%i_START.mat',basename,rank);
-    else
-                     f1 = sprintf('%s_rank%i_%0*i.nc', basename,rank, padsize, frameno);
-                     f2 = sprintf('%s_rank%i_%0*i.mat',basename,rank, padsize, frameno);
-    end
+    act = -1;
 
+    % Probably 1 of these
+    f1 = sprintf('%s_rank%i_%0*i.nc', basename,rank, padsize, frameno);
     if exist(f1, 'file') == 0;
-        if exist(f2, 'file') == 0
-            % try end
-            f3 = sprintf('%s_rank%i_FINAL.nc', basename,rank);
-            f4 = sprintf('%s_rank%i_FINAL.mat', basename,rank);
-            if exist(f3,'file') == 0
-                if exist(f4,'file') == 0
-                    error('Neither .mat or .nc with basename %s rank %i frame %i appears to exist.', basename, rank, frameno);
-                else
-                    % load final.mat
-                    fname = f4;
-                    act = 2;
-                end
-            else
-                % load final.nc
-                fname = f3;
-                act = 1;
-            end
-        else
+        f2 = sprintf('%s_rank%i_%0*i.mat',basename,rank, padsize, frameno);
+	if exist(f2, 'file') 
             % load .mat
             fname = f2;
             act = 2;
@@ -45,6 +26,41 @@ end
         fname = f1;
         act = 1;
     end
+
+    if (act < 0) && (frame == 0) % compat w/old versions
+    f1 = sprintf('%s_rank%i_START.nc', basename,rank);
+    if exist(f1, 'file') == 0;
+        f2 = sprintf('%s_rank%i_START.mat',basename,rank);
+	if exist(f2, 'file') 
+            % load .mat
+            fname = f2;
+            act = 2;
+        end
+    else
+        % load .nc
+        fname = f1;
+        act = 1;
+    end
+    end
+
+    if (act < 0) && (frame > 0)
+        % try end
+        f3 = sprintf('%s_rank%i_FINAL.nc', basename,rank);
+        f4 = sprintf('%s_rank%i_FINAL.mat', basename,rank);
+        if exist(f3,'file') == 0
+            if exist(f4,'file') 
+                % load final.mat
+                fname = f4;
+                act = 2;
+            end
+        else
+            % load final.nc
+            fname = f3;
+            act = 1;
+        end
+    end
+
+    if act < 0; error('No .mat or .nc with basename %s, rank %i, frame %i findable in pwd=''%s''\n', basename, rank, frameno, pwd()); end
 
     % Load the next frame into workspace
     try
