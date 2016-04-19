c7c7fa12fb189250e239712509c97889b3fac287
Erik Keever <ekeever1@uoregon.edu>
2016-04-18 19:28:48 -0700

    Add visualizer (with easy off-option) to RM runfile.

--- run/run_RichtmyerMeshkov.m
+++ run/run_RichtmyerMeshkov.m
@@ -13,6 +13,14 @@ run.activeSlices.xy = true;
 run.info            = 'Richtmyer-Meshkov instability test';
 run.notes           = '';
 
+rp = RealtimePlotter();
+  rp.plotmode = 4;
+  rp.plotDifference = 0;
+  rp.insertPause = 0;
+  rp.iterationsPerCall = 25;
+  rp.firstCallIteration = 1;
+run.peripherals{end+1} = rp;
+
 %--- Run tests ---%
 if (true)
     IC = run.saveInitialCondsToStructure();
