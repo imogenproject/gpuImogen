0b5fd82911b26249c734c7d85a6afe700ded53d3
Erik Keever <ekeever1@uoregon.edu>
2016-04-18 19:32:43 -0700

    Fix comment spacing

--- run/run_KelvinHelmholtz.m
+++ run/run_KelvinHelmholtz.m
@@ -1,14 +1,14 @@
-k% Run a test of the Kelvin-Helmholtz instability.
+% Run a test of the Kelvin-Helmholtz instability.
 
 %--- Initialize test ---%
 grid = [512 512 1];
 run                 = KelvinHelmholtzInitializer(grid);
 run.iterMax         = 1000;
 
-run.waveHeight      = .01;         % Creates sine waves at the boundaries of this amplitude (fraction of total grid)
+run.waveHeight      = .01;       % Creates sine waves at the boundaries of this amplitude (as fraction of total grid)
 run.numWave         = 5;         % Sine waves have this many peaks
-run.randAmp         = .01;         % X and Y velocity is given random components of this amplitude
-run.mach            = 2.0;        % Each region is given this speed, so total dv at the boundary is twice as large as this.
+run.randAmp         = .01;       % X and Y velocity is given random components of this amplitude
+run.mach            = 2.0;       % Each region is given this speed, so total velocity jump at the boundary is twice as large as this.
 run.timeMax         = (sqrt(5/3)/run.mach)*20;
 
 run.image.interval  = 50;
