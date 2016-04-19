e24e755ba5ca8a747be0a5d3f9f2ea79e76ab4f2
Erik Keever <ekeever1@uoregon.edu>
2016-04-18 19:41:08 -0700

    Various changes for versatility & better defaults

--- run/run_Advection.m
+++ run/run_Advection.m
@@ -1,11 +1,11 @@
 % Wave advection simulation
 
-%grid = [128 2 1];
-grid = [256 256 1];
+%grid = [512 512 1];
+grid = [512 1 1];
 
 %--- Initialize test ---%
 run             = AdvectionInitializer(grid);
-run.iterMax     = 100;
+run.iterMax     = 1000;
 run.info        = 'Advection test.';
 run.notes       = 'Simple advection test in the x-direction.';
 
@@ -27,28 +27,33 @@ run.backgroundMach = .45;
 % The MHD waves require a B to be set; Setting one is optional for the Entropy wave.
 % Any nonzero B will automatically activate magnetic fluxing
 run.waveType = 'sonic';
-run.amplitude = .5;
+run.amplitude = .25;
 % FWIW an amplitude of .0001 corresponds to a roughly 100dB sound in air
 %                      .01                    roughly 140dB
 
 % number of transverse wave periods in Y and Z directions
-run.wavenumber = [2 3 0];
+run.wavenumber = [3 0 0];
 % 1st method of setting run duration: normalized by cycle time
 %run.cycles = 1;
 % 2nd method of setting run duration: normalized by steepening critical time t*
-run.forCriticalTimes(5);
+run.forCriticalTimes(1.2);
 
 run.alias= 'sonic';
 
 run.ppSave.dim3 = 100;
 
 rp = RealtimePlotter();
-  rp.plotmode = 1;
+  rp.plotmode = 7;
   rp.plotDifference = 0;
-  rp.insertPause = 1;
-  rp.iterationsPerCall = 2;
+  rp.insertPause = 0;
+  rp.iterationsPerCall = 100;
   rp.firstCallIteration = 1;
-run.peripherals{end+1} = rp;
+%run.peripherals{end+1} = rp;
+fm = FlipMethod(); % 1 = HLL, 2 = HLLC, 3 = XJ
+  fm.iniMethod = 2; 
+%  fm.toMethod = 2;
+%  fm.atstep = -1;
+run.peripherals{end+1} = fm;
 
 run.waveLinearity(0);
 run.waveStationarity(0);
@@ -57,7 +62,7 @@ run.waveStationarity(0);
 if (true)
     IC = run.saveInitialCondsToStructure();
     outpath = imogen(IC);
-%    AdvectionAnalysis(outpath, 1);
-%    if mpi_amirank0(); fprintf('RUN STORED AT: %s\n', outpath); end
+    AdvectionAnalysis(outpath, 1)
+    if mpi_amirank0(); fprintf('RUN STORED AT: %s\n', outpath); end
 end
 
