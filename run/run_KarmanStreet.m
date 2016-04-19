120de760e4895fa70e597ad26a44b4afd9c1da56
Erik Keever <ekeever1@uoregon.edu>
2016-04-18 19:33:18 -0700

    Add visualizer info to Karman street sim.

--- run/run_KarmanStreet.m
+++ run/run_KarmanStreet.m
@@ -3,7 +3,7 @@
 %--- Initialize test ---%
 grid = [1024 512 1];
 run                 = KarmanStreetInitializer(grid);
-run.iterMax         = 400;
+run.iterMax         = 2000;
 
 run.image.interval  = 25;
 run.image.mass      = true;
@@ -12,6 +12,15 @@ run.info            = 'Karman Street test';
 run.notes           = '';
 run.ppSave.dim2     = 25;
 run.mach	    = .8;
+
+rp = RealtimePlotter();
+  rp.plotmode = 4;
+  rp.plotDifference = 0;
+  rp.insertPause = 0;
+  rp.iterationsPerCall = 20;
+  rp.firstCallIteration = 1;
+%run.peripherals{end+1} = rp;
+
 
 %--- Run tests ---%
 if (true)
