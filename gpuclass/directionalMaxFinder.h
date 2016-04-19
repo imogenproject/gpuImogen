d29352851b0c1d784a69fd65f8811bdeaa781f9c
Erik Keever <ekeever1@uoregon.edu>
2016-04-19 15:03:24 -0700

    Fix n00b errors in headers

--- gpuclass/directionalMaxFinder.h
+++ gpuclass/directionalMaxFinder.h
@@ -1,5 +1,8 @@
+#ifndef DIRECTIONALMAXFINDERH
+#define DIRECTIONALMAXFINDERH
+
 __global__ void cukern_DirectionalMax(double *d1, double *d2, double *out, int direct, int nx, int ny, int nz);
 __global__ void cukern_GlobalMax(double *din, int n, double *dout);
 __global__ void cukern_GlobalMax_forCFL(double *rho, double *cs, double *px, double *py, double *pz, int n, double *dout, int *dirOut);
 
-
+#endif
