677fec93367e82d8a8fd2852e1fadefa74424f8e
Erik Keever <ekeever1@uoregon.edu>
2016-05-28 21:38:52 -0700

    Move Einfeldt stuff to the Riemann Problem directory

---
+++ experiment/RiemannProblem/einfeldtFTSPlotter.m
@@ -0,0 +1,29 @@
+function einfeldtFTSPlotter(q)
+
+if isa(q,'struct')
+    x = q;
+else
+    load q;
+    x = TestResult.einfeldt;
+end
+q0 = 6
+
+figure(q0); hold on;
+figure(q0+1); hold on;
+
+for j = 1:numel(x.N)
+   S = SavefilePortal(x.paths{j});
+   F = S.jumpToLastFrame();
+  
+   xpts = ((1:x.N(j))' - (x.N(j)/2) + 0.5)*F.dGrid{1};
+   
+   figure(q0); plot(xpts,F.mass);
+   
+   figure(q0+1);
+   rhot = einfeldtSolution(xpts, 1, sqrt(1.4)*5.5, 1, 1.4, sum(F.time.history));
+   plot(xpts,rhot - F.mass);
+end
+
+
+
+end
