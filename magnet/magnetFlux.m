3344a1302ed067140e77c12c276ad17c7a5e0285
Erik Keever <ekeever1@uoregon.edu>
2016-08-03 00:27:38 -0700

    Update various old magnetism files.

--- magnet/magnetFlux.m
+++ magnet/magnetFlux.m
@@ -10,18 +10,15 @@ function magnetFlux(run, mass, mom, mag, X, magneticIndices)
 %>< mag                 Magnetic field array.                                   MagnetArray(3)
 %>> X                   Vector index along which to flux.                       int
 %>> magneticIndices     Index values for the magnetic components to flux        int(2)
-
-    GIS = GlobalIndexSemantics();
-
     for n=1:2
         I = magneticIndices(n); % Component of the b-field to flux.
 
         % Prepare arrays for magnetic flux step at cell corners (2nd order grid-aligned velocity)
 	mag(I).velGrid(X).array = cudaMagPrep(mom(X), mass, [I X]);
 
-%cudaHaloExchange(mag(I).velGrid(X).gputag, [1 2 3], X, GIS.topology, GIS.edgeInterior(:,X));
-        cudaHaloExchange(mag(I).velGrid(X), [1 2 3], X, GIS.topology, mag(I).bcHaloShare);
-        cudaHaloExchange(mag(I).velGrid(X), [1 2 3], I, GIS.topology, mag(I).bcHaloShare);
+%cudaHaloExchange(mag(I).velGrid(X).gputag, [1 2 3], X, run.geometry.topology, GIS.edgeInterior(:,X));
+        cudaHaloExchange(mag(I).velGrid(X), [1 2 3], X, run.geometry.topology, mag(I).bcHaloShare);
+        cudaHaloExchange(mag(I).velGrid(X), [1 2 3], I, run.geometry.topology, mag(I).bcHaloShare);
 
         % Flux Advection step
         relaxingMagnet(run, mag, mag(I).velGrid(X), X, I);
@@ -31,12 +28,12 @@ function magnetFlux(run, mass, mom, mag, X, magneticIndices)
 
     for n = 1:3
         mag(n).updateCellCentered();
- %       mag(X).updateCellCentered();
+ %       mag(X).updateCellCentered(); % isn't this right?
     end
 
-%    cudaHaloExchange(mag(X).cellMag.gputag, [1 2 3], I, GIS.topology, GIS.edgeInterior(:,I));
-%    cudaHaloExchange(mag(X).cellMag.gputag, [1 2 3], X, GIS.topology, GIS.edgeInterior(:,X));
-%    cudaHaloExchange(mag(I).cellMag.gputag, [1 2 3], I, GIS.topology, GIS.edgeInterior(:,I));
-%    cudaHaloExchange(mag(I).cellMag.gputag, [1 2 3], X, GIS.topology, GIS.edgeInterior(:,X));
+%    cudaHaloExchange(mag(X).cellMag.gputag, [1 2 3], I, run.geometry.topology, GIS.edgeInterior(:,I));
+%    cudaHaloExchange(mag(X).cellMag.gputag, [1 2 3], X, run.geometry.topology, GIS.edgeInterior(:,X));
+%    cudaHaloExchange(mag(I).cellMag.gputag, [1 2 3], I, run.geometry.topology, GIS.edgeInterior(:,I));
+%    cudaHaloExchange(mag(I).cellMag.gputag, [1 2 3], X, run.geometry.topology, GIS.edgeInterior(:,X));
 
 end
--- magnet/relaxingMagnet.m
+++ magnet/relaxingMagnet.m
@@ -12,14 +12,12 @@ function relaxingMagnet(run, mag, velGrid, X, I)
     %-----------------------------------------------------------------------------------------------
     % Initialization
     %---------------
-    fluxFactor = 0.5*run.time.dTime ./ run.DGRID{X};
-    GIS = GlobalIndexSemantics();
+    fluxFactor = 0.5*run.time.dTime ./ run.geometry.d3h(X);
 
     %+++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     %Half-Timestep predictor step (first-order upwind,not TVD)
     %+++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    [mag(I).store(X).array velocityFlow] = cudaMagW(mag(I), velGrid, fluxFactor, X);
-
+    [mag(I).store(X).array, velocityFlow] = cudaMagW(mag(I), velGrid, fluxFactor, X);
 %saveDEBUG(mag(I).store(X).array,sprintf('magI w after w upwind'));
 
     %+++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -31,15 +29,15 @@ function relaxingMagnet(run, mag, velGrid, X, I)
     magflux = mag(I).flux(X);
     [magflux.array] = cudaMagTVD(mag(I).store(X), mag(I), velGrid, fluxFactor, X);
     % This returns the flux array, pre-shifted forward one in the X direction to avoid the shift originally present below
-    cudaHaloExchange(mag(I),  [1 2 3], X, GIS.topology, mag(I).bcHaloShare);
-    cudaHaloExchange(magflux, [1 2 3], X, GIS.topology, mag(I).bcHaloShare);
+    cudaHaloExchange(mag(I),  [1 2 3], X, run.geometry.topology, mag(I).bcHaloShare);
+    cudaHaloExchange(magflux, [1 2 3], X, run.geometry.topology, mag(I).bcHaloShare);
 
     mag(I).applyBoundaryConditions(X);
     
     %-----------------------------------------------------------------------------------
     % Reuse advection flux for constraint step for CT
     %------------------------------------------------
-    fluxFactor = run.time.dTime ./ run.DGRID{I};
+    fluxFactor = run.time.dTime ./ run.geometry.d3h(I);
 
     cudaFwdDifference(mag(X), magflux, I, fluxFactor);
     mag(X).applyBoundaryConditions(I);
@@ -47,7 +45,5 @@ function relaxingMagnet(run, mag, velGrid, X, I)
 
     % FIXME: fix this braindead hack
 %    ec = double([strcmp(mag(X).bcModes{1,I},'circ'); strcmp(mag(X).bcModes{2,I},'circ')]);
-    cudaHaloExchange(mag(X), [1 2 3], I, GIS.topology,mag(X).bcHaloShare);
-
-
+    cudaHaloExchange(mag(X), [1 2 3], I, run.geometry.topology,mag(X).bcHaloShare);
 end
