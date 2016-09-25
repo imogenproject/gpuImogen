2cc265908855451517e6f80f3e99741f39b93122
Erik Keever <ekeever1@uoregon.edu>
2016-09-24 21:09:38 -0700

    In the event checkpointing is used, make use of diffusion easier to control.

--- save/restoreCheckpoint.m
+++ save/restoreCheckpoint.m
@@ -9,33 +9,35 @@ run.time.iteration = dump.iteration;
 run.time.time      = dump.oldTime;
 run.time.CFL       = 0.75*run.time.CFL;
 
+DIFFUSE=0;
+
 ctr = 1;
 for a = 1:numel(run.fluid)
     DH = run.fluid(a).DataHolder;
-    
-    rho = dump.arrays{ctr};
-    ener= dump.arrays{ctr+1};
-    px  = dump.arrays{ctr+2};
-    py  = dump.arrays{ctr+3};
-    pz  = dump.arrays{ctr+4};
-    
-    P = (run.GAMMA - 1) * (ener - .5*(px.^2+py.^2+pz.^2)./rho);
-    
-    dconstant = 0.3;
-    
-    px = px + dconstant * del2(px);
-    py = py + dconstant * del2(py);
-    pz = pz + dconstant * del2(pz);
-    rho= rho+ dconstant * del2(rho);
-    P  = P  + dconstant * del2(P);
-    ener = .5*(px.^2+py.^2+pz.^2)./rho + P/(run.GAMMA-1);
-    
-    dump.arrays{ctr} = rho;
-    dump.arrays{ctr+1} = ener;
-    dump.arrays{ctr+2} = px;
-    dump.arrays{ctr+3} = py;
-    dump.arrays{ctr+4} = pz;
-    
+    if DIFFUSE
+        rho = dump.arrays{ctr};
+        ener= dump.arrays{ctr+1};
+        px  = dump.arrays{ctr+2};
+        py  = dump.arrays{ctr+3};
+        pz  = dump.arrays{ctr+4};
+        
+        P = (run.GAMMA - 1) * (ener - .5*(px.^2+py.^2+pz.^2)./rho);
+        
+        dconstant = 0.3;
+        
+        px = px + dconstant * del2(px);
+        py = py + dconstant * del2(py);
+        pz = pz + dconstant * del2(pz);
+        rho= rho+ dconstant * del2(rho);
+        P  = P  + dconstant * del2(P);
+        ener = .5*(px.^2+py.^2+pz.^2)./rho + P/(run.GAMMA-1);
+        
+        dump.arrays{ctr} = rho;
+        dump.arrays{ctr+1} = ener;
+        dump.arrays{ctr+2} = px;
+        dump.arrays{ctr+3} = py;
+        dump.arrays{ctr+4} = pz;
+    end
     
     for b = 0:4
         ignoreit = GPU_setslab(DH, b, dump.arrays{ctr});
