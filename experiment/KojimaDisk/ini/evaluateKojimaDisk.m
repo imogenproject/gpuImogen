624af6b575f7afb62d8680ed13440bf36160097a
Erik Keever <ekeever1@uoregon.edu>
2016-09-24 21:27:53 -0700

    Latest work on Kojima disk initializer... this in concert with cylindrical coords, bug-free HLL, and the VTO sort of almost works

--- experiment/KojimaDisk/ini/evaluateKojimaDisk.m
+++ experiment/KojimaDisk/ini/evaluateKojimaDisk.m
@@ -33,52 +33,60 @@ function [rho, moma, momb, eint] = evaluateKojimaDisk(q, gamma, radiusRatio, sta
     isPartOfDisk       = (bernoulli > 0);
     bernoulli(~isPartOfDisk) = 0;
 
-    rho                = max((bernoulli * (gamma-1)/gamma).^(1/(gamma-1)), lomass*1.1 * ENUM.GRAV_FEELGRAV_COEFF);
+    rho                = max((bernoulli * (gamma-1)/gamma).^(1/(gamma-1)), lomass);
     rho(isPartOfDisk)  = max(rho(isPartOfDisk), lomass * ENUM.GRAV_FEELGRAV_COEFF);
+
     eint = zeros(size(rho));
-    
     eint(isPartOfDisk) = rho(isPartOfDisk).^gamma / (gamma-1);
     
-    %--- Compute momentum distribution ---%
-    %        Apply Kojima momentum to the vast bulk of the disk, apply a blurred function to the
-    %        inner and outer perimeters.
-    cextern=.1;
-    
-    rbdy = 1.01*diskInfo.rin;
-    rhobdy=  c1 + (1 ./ rbdy) + (hsq / xq) * rbdy^xq;
-    rhobdy = (rhobdy*(gamma-1)/gamma)^(1/(gamma-1));
-    
-    rhovac = minRhoCoeff * diskInfo.rhomax;
-    
-    efact = -hsq./rPoints + 1./starDistance + hsq/rbdy - 1/rbdy;
-    efact = efact / cextern^2;
-    
-    outside = (rho < rhobdy);
-    
-    rho(outside) = rhovac; %rhobdy*exp(efact(outside));
-    eint(outside) = rhovac * cextern^2 / (gamma*(gamma-1));
-    
-    % compute velocity, then mult by density...
     mom                = rPoints.^(1-q);
-    mom(outside) = 0; %(rPoints(outside).^-.5);
+
+    %--- Now begin the numeric fudging ---%
+
+    % Set an external soundspeed for finite temperature: .01 yields T = 1e-4... ok
+    cextern=.01;
+    
+    if 0 % This embeds an isothermal Keplerian disk outside the Kojima disk in an attempt
+         % to reduce the startup transient. It doesn't work very well.
+        rbdy = 1.01*diskInfo.rin;
+        rhobdy=  c1 + (1 ./ rbdy) + (hsq / xq) * rbdy^xq;
+        rhobdy = (rhobdy*(gamma-1)/gamma)^(1/(gamma-1));
+    
+        efact = -hsq./rPoints + 1./starDistance + hsq/rbdy - 1/rbdy;
+        efact = efact / cextern^2;
+
+        outside = (rho == lomass);
+
+        mom(outside) = rPoints(outside).^-.5;
+
+        rho(outside) = rhobdy*exp(efact(outside));
+        rho(rho < lomass) = lomass; % prevent *excessive* vacuumeyness
+
+        eint(outside) = rho(outside) * cextern^2 / (gamma*(gamma-1));
+    end
+
+    % Make sure velocity is zero where it ought be zero
+    outside = (rho == lomass);
+    mom(outside) = 0;
    
-    mom = mom .* rho; % convert v to p
+    % Convert to momentum density
+    mom = mom .* rho;
     
-    % turn off where g doesnt ppulll
-    mom(rho < minRhoCoeff*4*diskInfo.rhomax) = 0;
+    % Switch momentum off where gravity won't act
+    % FIXME: this and ENUM.GRAV_FEELGRAV_COEFF do not sync with sources/source.m
+    mom(rho < 4*lomass) = 0;
     
-    
-%    mom(rPoints < 0.3) = 0;
-%    rho(rPoints < 0.3) = minRhoCoeff*diskInfo.rhomax;
-%    eint(rPoints< 0.3) = minRhoCoeff*diskInfo.rhomax * cextern^2 / (gamma*(gamma-1));
-    dr = rPoints(2)-rPoints(1); % hack hack hack
-if 0
-    mu = 2; nu = 2;
+    if 0 % Another bad attempt to reduce the inner edge transient (by making it just fall in)
+         % This, too, does not work very well.
+        dr = rPoints(2)-rPoints(1);
+        mu = 2; nu = 2;
 
-    bz = (rPoints - radiusRatio > -mu*dr) & (rPoints - radiusRatio < (nu-mu)*dr);
+        bz = (rPoints - radiusRatio > -mu*dr) & (rPoints - radiusRatio < (nu-mu)*dr);
 
-    mom(bz) = rPoints(bz).^(1-q) .* rho(bz) .* (rPoints(bz) - radiusRatio + mu*dr) / (nu*dr); 
-end
+        mom(bz) = rPoints(bz).^(1-q) .* rho(bz) .* (rPoints(bz) - radiusRatio + mu*dr) / (nu*dr); 
+    end
+
+    % momentum is projected into x/y or r/theta space. mom_z is always zero.
     if geomode == ENUM.GEOMETRY_CYLINDRICAL
         momb = mom;
         moma = zeros(size(mom));
@@ -86,8 +94,6 @@ end
         moma = -mom .* sin(phiPoints);
         momb = mom .* cos(PhiPoints);
     end
-
-
 
 end
 
