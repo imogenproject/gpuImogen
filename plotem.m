6ae9477a9ddce1a2b13f96b0c658fc72ca6b98a2
Erik Keever <ekeever1@uoregon.edu>
2017-01-07 14:38:55 -0800

    Improve the plotem.m debug helper to automatically pick between using the mass/mom/ener variables and extracting them from run.().

--- plotem.m
+++ plotem.m
@@ -1,16 +1,48 @@
-% This is a terrible function mainly meant to provide instant freedback during debugging
+% This is a terrible function mainly meant to provide instant feedback during debugging
 
+p2 = 3;
+
+if exist('run')
+
+
+if numel(find(size(run.fluid(1).mass.array) > 1)) > 1 % 2d
+    eint = run.fluid(1).ener.array - .5*(run.fluid(1).mom(1).array.^2+run.fluid(1).mom(2).array.^2+run.fluid(1).mom(3).array.^2)./run.fluid(1).mass.array;
+    subplot(2,2,3); imagesc(squeeze(log10(run.fluid(1).mass.array))); title('log10[mass]'); colorbar;
+    subplot(2,2,1); imagesc(squeeze(run.fluid(1).mom(1).array./run.fluid(1).mass.array)); title('v(1)'); colorbar;
+    subplot(2,2,2); imagesc(squeeze(run.fluid(1).mom(2).array./run.fluid(1).mass.array)); title('v(2)'); colorbar;
+    subplot(2,2,4); imagesc(squeeze(eint./run.fluid(1).mass.array)); title('T'); colorbar;
+else
+    if numel(run.fluid) > 1; MF = 1; else; MF = 0; end
+    
+    if MF; for f = 1:4; subplot(2,2,f); hold off; end; end
+    
+    for f = 1:numel(run.fluid)
+        S = run.fluid(f); % S for species
+        eint = S.ener.array - .5*(S.mom(1).array.^2+S.mom(2).array.^2+S.mom(3).array.^2)./S.mass.array;
+        
+        subplot(2,2,3); plot(log10(S.mass.array)); title('log10[mass]');
+        subplot(2,2,1); plot(S.mom(1).array./S.mass.array); title('v(1)');
+        subplot(2,2,2); plot(S.mom(2).array./S.mass.array); title('v(2)');
+        subplot(2,2,4); plot(eint./S.mass.array); title('Temperature');
+        if MF && (f == 1); for f = 1:4; subplot(2,2,f); hold on; end; end
+    end
+end
+    
+else
+    
 eint = ener.array - .5*(mom(1).array.^2+mom(2).array.^2+mom(3).array.^2)./mass.array;
 
 if numel(find(size(mass.array) > 1)) > 1 % 2d
-    subplot(2,2,3); imagesc(mass.array); title('mass'); colorbar;
-    subplot(2,2,1); imagesc(mom(1).array); title('mom(1)'); colorbar;
-    subplot(2,2,2); imagesc(mom(2).array); title('mom(2)'); colorbar;
-    subplot(2,2,4); imagesc(.6666*eint); title('Pressure'); colorbar;
+    subplot(2,2,3); imagesc(log10(mass.array)); title('log10 mass'); colorbar;
+    subplot(2,2,1); imagesc(mom(1).array./mass.array); title('v(1)'); colorbar;
+    subplot(2,2,2); imagesc(mom(p2).array./mass.array); title('v(2)'); colorbar;
+    subplot(2,2,4); imagesc(log10(eint)); title('log10[Einternal]'); colorbar;
 else
-    subplot(2,2,3); plot(mass.array); title('mass');
+    subplot(2,2,3); plot(log10(mass.array)); title('mass');
     subplot(2,2,1); plot(mom(1).array); title('mom(1)');
     subplot(2,2,2); plot(mom(2).array); title('mom(2)');
-    subplot(2,2,4); plot(.6666*eint); title('Pressure');
+    subplot(2,2,4); plot(eint./mass.array); title('T');
+
 end
 
+end
