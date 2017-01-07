ad1fb15e69f0e963b5dcb149c2eee17a30bce6d5
Erik Keever <ekeever1@uoregon.edu>
2017-01-07 14:37:04 -0800

    Move the fluid step debugging harness into its own file; Now supports debugging both HLL and HLLC kernels.

--- gpuclass/debug_inserts.h
+++ gpuclass/debug_inserts.h
@@ -1,7 +1,8 @@
-#include "cudaCommon.h"
-#include "mex.h"
 
-#warning "WARNING: COMPILING cudaFluidStep() WITH DEBUG ENABLED. cudaFluidStep will require an output argument to dump to!"
+#warning "WARNING: COMPILING cudaFluidStep() WITH DEBUG ENABLED; cudaFluidStep() WILL REQUIRE OUTPUT ARGS TO DUMP TO"
+#warning "WARNING: THIS FUNCTION WILL ONLY BE COMPATIBLE WITH STANDALONE OPERATION. YOU MUST MODIFY imogen.m:107 TO CALL flux()"
+#warning "WARNING: AND MODIFY relaxingFluid.m TO ACCEPT RETURN RETURN ARGUMENT & DBSTOP THERE"
+#warning "WARNING: IF YOU ARE NOT DEVELOPING THE CODE AND THIS WARNING HAPPENS, AN UNACCEPTABLY BROKEN COMMIT HAS HAPPENED!"
 
 // If defined, the code runs the Euler prediction step and copies wStepValues back to the Matlab fluid data arrays
 // If not defined, it runs the RK2 predictor/corrector step
@@ -17,9 +18,10 @@
 #define DBG_NUMARRAYS 6
 
 #ifdef DBG_FIRSTORDER
-#define DBGSAVE(n, x) if(thisThreadDelivers) { Qout[((n)+6)*DEV_SLABSIZE] = (x); }
+#define DBGSAVE(n, x)     if(thisThreadDelivers) { Qout[((n)+6)*DEV_SLABSIZE] = (x); }
+#define DBSAVE_HLL(n, x)  if(thisThreadDelivers) { Qstore[((n)+6)*DEV_SLABSIZE] = (x); }
 #else
-#define DBGSAVE(n, x) if(thisThreadDelivers) {  Qin[((n)+6)*DEV_SLABSIZE] = (x); }
+#define DBGSAVE(n, x)     if(thisThreadDelivers) {  Qin[((n)+6)*DEV_SLABSIZE] = (x); }
 #endif
 
 // Assuming debug has been put on the wStepValues array, download it to a Matlab array
@@ -52,8 +54,10 @@ void returnDebugArray(MGArray *ref, int x, double **dbgArrays, mxArray *plhs[])
 	MGArray scratch = ref[0];
 	// wStepValues is otherwise identical so overwrite the new one's pointers
 	int j, k;
-	for(j = 0; j < scratch.nGPUs; j++) scratch.devicePtr[j] = dbgArrays[j];
+	// Skip to the debug-containing arrays by adding 6...
+	for(j = 0; j < scratch.nGPUs; j++) scratch.devicePtr[j] = dbgArrays[j] + (6*scratch.slabPitch[j])/8;
 
+	// Now dump them out one by one.
 	for(k = 0; k < x; k++) {
 		// download
 		MGA_downloadArrayToCPU(&scratch, &result, 0);
