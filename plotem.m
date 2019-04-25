4fa08fef6111cc93035b0bffd8df6bd9515f1cc2
Erik Keever <ekeever1@uoregon.edu>
2019-04-24 17:59:45 -0700

    Make it easier to change the awful plotem.m function between linear & log scale.

--- plotem.m
+++ plotem.m
@@ -1,6 +1,7 @@
 % This is a terrible function mainly meant to provide instant feedback during debugging
 
 p2 = 3;
+logger = @(x) x;
 
 if exist('run','var')
     
@@ -11,10 +12,10 @@ if exist('run','var')
     end
     
     thingA = run.fluid(fn).mom(1).array./run.fluid(fn).mass.array;
-    thingB = run.fluid(fn).mom(2).array./run.fluid(fn).mass.array;
+    thingB = run.fluid(fn).mom(p2).array./run.fluid(fn).mass.array;
     
-    thingA = log10(abs(thingA));
-    thingB = log10(abs(thingB));
+    thingA = ((thingA));
+    thingB = ((thingB));
     
     if numel(find(size(run.fluid(fn).mass.array) > 1)) > 1 % 2d
         
@@ -25,13 +26,13 @@ if exist('run','var')
                 z0 = round(size(run.fluid(fn).mass.array,3)/2);
             end
             eint = run.fluid(fn).ener.array(:,:,z0) - .5*(run.fluid(fn).mom(1).array(:,:,z0).^2+run.fluid(fn).mom(2).array(:,:,z0).^2+run.fluid(fn).mom(3).array(:,:,z0).^2)./run.fluid(fn).mass.array(:,:,z0);
-            subplot(2,2,3); imagesc(squeeze(log10(run.fluid(fn).mass.array(:,:,z0)))); title('log10[mass]'); colorbar;
+            subplot(2,2,3); imagesc(squeeze(logger(run.fluid(fn).mass.array(:,:,z0)))); title('log10[mass]'); colorbar;
             subplot(2,2,1); imagesc(squeeze(thingA(:,:,z0))); title('v(1)'); colorbar;
             subplot(2,2,2); imagesc(squeeze(thingB(:,:,z0))); title('v(2)'); colorbar;
             subplot(2,2,4); imagesc(squeeze(eint./run.fluid(fn).mass.array(:,:,z0))); title('T'); colorbar;
         else
             eint = run.fluid(fn).ener.array - .5*(run.fluid(fn).mom(1).array.^2+run.fluid(fn).mom(2).array.^2+run.fluid(fn).mom(3).array.^2)./run.fluid(fn).mass.array;
-            subplot(2,2,3); imagesc(squeeze(log10(run.fluid(fn).mass.array))); title('log10[mass]'); colorbar;
+            subplot(2,2,3); imagesc(squeeze(logger(run.fluid(fn).mass.array))); title('log10[mass]'); colorbar;
             subplot(2,2,1); imagesc(squeeze(thingA)); title('v(1)'); colorbar;
             subplot(2,2,2); imagesc(squeeze(thingB)); title('v(2)'); colorbar;
             subplot(2,2,4); imagesc(squeeze(eint./run.fluid(fn).mass.array)); title('T'); colorbar;
@@ -45,9 +46,9 @@ if exist('run','var')
             S = run.fluid(f); % S for species
             eint = S.ener.array - .5*(S.mom(1).array.^2+S.mom(2).array.^2+S.mom(3).array.^2)./S.mass.array;
             
-            subplot(2,2,3); plot(log10(S.mass.array)); title('log10[mass]');
+            subplot(2,2,3); plot(logger(S.mass.array)); title('log10[mass]');
             subplot(2,2,1); plot(S.mom(1).array./S.mass.array); title('v(1)');
-            subplot(2,2,2); plot(S.mom(2).array./S.mass.array); title('v(2)');
+            subplot(2,2,2); plot(S.mom(p2).array./S.mass.array); title('v(2)');
             subplot(2,2,4); plot(eint./S.mass.array); title('Temperature');
             if MF && (f == 1); for g = 1:4; subplot(2,2,g); hold on; end; end
         end
@@ -58,14 +59,14 @@ else
     eint = ener.array - .5*(mom(1).array.^2+mom(2).array.^2+mom(3).array.^2)./mass.array;
     
     if numel(find(size(mass.array) > 1)) > 1 % 2d
-        subplot(2,2,3); imagesc(log10(mass.array)); title('log10 mass'); colorbar;
+        subplot(2,2,3); imagesc(logger(mass.array)); title('log10 mass'); colorbar;
         subplot(2,2,1); imagesc(mom(1).array./mass.array); title('v(1)'); colorbar;
         subplot(2,2,2); imagesc(mom(p2).array./mass.array); title('v(2)'); colorbar;
-        subplot(2,2,4); imagesc(log10(eint)); title('log10[Einternal]'); colorbar;
+        subplot(2,2,4); imagesc(logger(eint)); title('log10[Einternal]'); colorbar;
     else
-        subplot(2,2,3); plot(log10(mass.array)); title('mass');
+        subplot(2,2,3); plot(logger(mass.array)); title('mass');
         subplot(2,2,1); plot(mom(1).array); title('mom(1)');
-        subplot(2,2,2); plot(mom(2).array); title('mom(2)');
+        subplot(2,2,2); plot(mom(p2).array); title('mom(2)');
         subplot(2,2,4); plot(eint./mass.array); title('T');
         
     end
