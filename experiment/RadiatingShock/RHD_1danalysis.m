8cbab6efd37abc302a4a44d0d3b305d0afc992ac
Erik Keever <ekeever1@uoregon.edu>
2019-06-28 16:35:52 -0700

    Improvements to 1D radiative shock analyzer

--- experiment/RadiatingShock/RHD_1danalysis.m
+++ experiment/RadiatingShock/RHD_1danalysis.m
@@ -2,7 +2,16 @@ load 4D_XYZT
 
 F = DataFrame(F);
 
-x = trackFront2(squeeze(F.mass(1:2250,1,1,:)), (1:4096)*F.dGrid{1}, 2.5);
+x = trackFront2(squeeze(F.mass), (1:size(F.mass,1))*F.dGrid{1}, 2.5);
+
+imagesc(diff(squeeze(F.mass), 1, 2)./squeeze(F.mass(:,1,1,1:(end-1))));
+thefig = gca();
+thefig.CLim = [-.3 .3];
+
+pts = input('Identify frames demarking one round-trip: ');
+
+tfunda = F.time.tFrame(pts);
+ffunda = 1/(tfunda(2)-tfunda(1))
 
 plot(x);
 
@@ -11,7 +20,7 @@ nlpoint = input('Saturation of nonlinearity frame? ');
 ttotal = cumsum(F.time.history);
 ttotal = [0; ttotal(1:800:end)]; % hack: assumption!
 
-timepts = ttotal(nlpoint:end);
+timepts = F.time.tFrame(nlpoint:end);
 pospts = x(nlpoint:end)';
 
 [coeffs, resid] = polyfit(timepts, pospts, 1)
@@ -31,12 +40,17 @@ tfunda = timepts(end) - timepts(1);
 
 plot((1:xi)/tfunda, xf(2:end/2));
 
+peaks = input('Input list of spectral peaks: ');
+
+disp('F/F_round trip: ')
+disp(peaks / ffunda)
+
 dump = input('enter to continue');
 
 ntime = size(F.mass,4);
 
 for N = (ntime-25):ntime
-	plot(squeeze(F.mass(:,1,1,N))); title(N);
+	plot(log(squeeze(F.mass(:,1,1,N)))); title(N);
 	dump = input('enter to cont');
 end
 
