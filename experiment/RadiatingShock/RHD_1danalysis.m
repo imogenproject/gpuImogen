72e581ad8b297e66ba3620f08c222bf9459bc667
Erik Keever <ekeever1@uoregon.edu>
2019-07-10 16:23:36 -0700

    Improvements to RHD analyzer yet again.

--- experiment/RadiatingShock/RHD_1danalysis.m
+++ experiment/RadiatingShock/RHD_1danalysis.m
@@ -4,18 +4,15 @@ F = DataFrame(F);
 
 x = trackFront2(squeeze(F.mass), (1:size(F.mass,1))*F.dGrid{1}, .5*(F.gamma+1)/(F.gamma-1));
 
-hold off;
+yyaxis right; hold off; cla('reset');
+yyaxis left; hold off; cla('reset');
+
 imagesc(diff(squeeze(F.mass), 1, 2)./squeeze(F.mass(:,1,1,1:(end-1))));
 thefig = gca();
 thefig.CLim = [-.3 .3];
-drawnow;
-hold off;
-drawnow;
 
 vx = squeeze(F.velX);
 cs = squeeze(sqrt(F.gamma*F.pressure ./ F.mass));
-
-
 
 pts = input('Identify frames demarking one round-trip: ');
 
@@ -25,6 +22,8 @@ pts(2) = RHD_utils.walkdown(x, pts(2), 5);
 % This block projects parabolas onto the left and right areas indicated
 % and takes their nearby intercept as the true shock bounce point;
 % This is much better than the nearest-guess method previously used.
+
+cla('reset');
 
 plot((pts(1)-10):(pts(2)+10), x((pts(1)-10):(pts(2)+10)));
 hold on;
@@ -76,7 +75,13 @@ else
     endpt = numel(F.time.time);
 end
     
+spacing = round(pts(2)-pts(1));
+cycles = round(endpt-nlpoint(1))/(interframe2 - interframe1) - 1;
 
+endpt = RHD_utils.walkdown(x, round(nlpoint(1) + cycles*(interframe2-interframe1)), spacing);
+stpt = RHD_utils.walkdown(x, nlpoint(1), spacing);
+
+if 0
 npc = nlpoints_class(x(1:endpt), F.time.time(1:endpt), nlpoint);
 
 timepts = F.time.time(nlpoint:endpt);
@@ -89,6 +94,11 @@ plot(timepts, oscil);
 dump = input('Use up/down arrow to get the endpoints to match & minimize spectral garbage, then press enter: ');
 stpt = npc.leftpoint;
 clear npc;
+end
+
+npt = 1+endpt-stpt;
+
+if npt/2 ~= round(npt/2); stpt = stpt-1; end
 
 timepts = F.time.time(stpt:endpt);
 pospts = x(stpt:endpt)';
@@ -101,25 +111,42 @@ xfourier = log(abs(fft(oscil)));
 xi = numel(xfourier(2:end/2));
 
 tfunda = (timepts(end) - timepts(1));
-% Generate harmonic markers
+% Generate harmonic markersnumel(xfori
 fquery = (1:16)*ffunda;
 ypt = interp1((1:xi)/tfunda, xfourier(2:end/2), fquery, 'linear');
 
-plot((1:xi)/tfunda, exp(xfourier(2:end/2)));
-hold on
+hold off;
+yyaxis left;
 
-rth = input('Radiative theta? ');
+plot((1:xi)/tfunda, exp(xfourier(2:end/2)),'b-');
+hold on
+plot(fquery, exp(ypt), 'bo','markersize',7);
+
+% Determine radiative theta from the directory name because I'm sick of
+% entering it by hand
+thedir = pwd();
+thedir = thedir(1:(end-7));
+chrs = find(thedir=='_');
+rth = .01 * sscanf(thedir((max(chrs)+6):end), '%i');
+
+% Compute the luminosity on the given interval, normalized by L(t=0)
 rr = RHD_utils.computeRelativeLuminosity(F, rth);
 
-rft = fft(rr(stpt:endpt));
+rft = 2*fft(rr(stpt:endpt)) / (1+endpt-stpt);
 
-plot((1:xi)/tfunda,abs(rft(2:end/2)));
+yyaxis right; 
+plot((1:xi)/tfunda,abs(rft(2:end/2)), 'r-');
+ypt = interp1((1:xi)/tfunda, abs(rft(2:end/2)), fquery, 'linear');
 
-plot(fquery, exp(ypt), 'rx');
+plot(fquery, (ypt), 'rx','markersize',7);
 
 nneighbor = round(fquery*tfunda);
 
 ispeak = @(y, xi) (y(xi) > y(xi+1)) & (y(xi) > y(xi-1)) & (y(xi-1) > y(xi-2)) & (y(xi+1) > y(xi+2));
+
+grid on;
+
+yyaxis left;
 
 fatdot = [];
 for n = 1:16
@@ -133,6 +160,7 @@ for n = 1:16
         fatdot(end+1,:) = [p-1, xfourier(p)];
     end
 end
+
 if size(fatdot,1) > 0
     plot(fatdot(:,1)/tfunda, exp(fatdot(:,2)), 'kv', 'MarkerSize', 8);
     
@@ -142,10 +170,10 @@ if size(fatdot,1) > 0
         fi = fatdot(sortidx(n),1)/tfunda;
        fprintf('Spectral peak at f=%f = %f f0 has magnitude %f\n', fi, fi / ffunda, exp( fatdot(sortidx(n),2)));
     end
-    legend('X_{shock} spectrum', 'Relative luminosity spectrum', 'Harmonics of indicated period', 'Possible base tones');
+    legend('X_{shock} spectrum',  'Harmonics of indicated period', 'Possible base tones', 'Relative luminosity spectrum');
 else
     disp('Well this is embarassing; No multiples of Ffunda hit a spectral peak...');
-    legend('Measured spectrum', 'harmonics of user-input period');
+    legend('X_{shock} spectrum',  'Harmonics of indicated period', 'Relative luminosity spectrum');
 end
 
 hold off;
@@ -153,6 +181,7 @@ hold off;
 peaks = input('Input list of other spectral peaks of interest if desired: ');
 
 pwd
+
 disp('F/F_round trip: ')
 disp(peaks / ffunda)
 disp(ffunda)
